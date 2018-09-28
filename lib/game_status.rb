f90b7245092f2a2e4628d90d680b6769ebfca937
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-28 12:28:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,71 @@
+require 'pry'
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # First diagonal
+  [2,4,6] # Second diagonal
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each {|combination|
+    index_0 = combination[0]
+    index_1 = combination[1]
+    index_2 = combination[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combination
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? { |index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
